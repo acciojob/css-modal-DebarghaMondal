5c4f4ced42077aa8bd88d78ab4c7c808fee59926
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-06-30 17:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+describe('Modal Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should open modal when button is clicked', () => {
+    cy.get('#openModal').click();
+    cy.get('#modal').should('have.class', 'open');
+  });
+
+  it('should close modal when close button is clicked', () => {
+    cy.get('#openModal').click();
+    cy.get('.close-modal').click();
+    cy.get('#modal').should('not.have.class', 'open');
+  });
+
+  it('should close modal when clicking outside of modal', () => {
+    cy.get('#openModal').click();
+    cy.get('body').click('topRight'); // Click outside the modal area
+    cy.get('#modal').should('not.have.class', 'open');
+  });
+});
